ffc4a439032395754b8e8a7b72dba526650bb1dd
Rakshit kamboj <69065683+rakshitkamboj@users.noreply.github.com>
2021-02-27 23:57:00 +0530

    day27 done

---
+++ day27/Task_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'Task_tile.dart';
+import 'package:todoey_flutter/modal/tasks.dart';
+
+class TasksList extends StatefulWidget {
+  final List<Task> tasks;
+  TasksList(this.tasks);
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+            taskTitle: widget.tasks[index].name,
+            isChecked: widget.tasks[index].isDone,
+            checkBoxCallback: (bool checkboxState) {
+              setState(() {
+                widget.tasks[index].toggleDone();
+              });
+            });
+      },
+      itemCount: widget.tasks.length,
+    );
+  }
+}
